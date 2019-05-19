078ca2b0e05997a1d14c68bf94a89198230dcc3d
mladen2004 <48154793+mladen2004@users.noreply.github.com>
2019-05-19 18:13:46 +0200

    Product Model and InMemory Product Repository Built

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product//deklariramo je kao public jer mora biti dostupna iz drugih projekata, sadrži informacije o proizvodima
+    {
+        public string Id { get; set; }
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0,1000)]
+        public decimal Price { get; set; }
+        public string Categoriy { get; set; }
+        public string Image { get; set; }//sadržavat će url na product image
+
+        public Product()//product konstruktor
+        {
+            this.Id = Guid.NewGuid().ToString();//kod svakog kereiranja novog proizvoda generirati će se novi guid
+
+        }
+
+    }
+}
---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;//dodajemo reference na navedena dva elementa
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+
+        List<Product> products=new List<Product>();
+
+        //kreiranje konstruktora
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            //ako je cache prazan
+            if (products == null)
+            {
+                //kreiramo novu praznu listu
+                products = new List<Product>();
+            }
+        }
+
+            //metoda za pohranu liste u cache
+       public void Commit()
+       {
+             cache["products"] = products;
+       }
+        //metoda za dodavanje novog proizvoda u listu
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+        //update metoda
+        public void Update(Product product)
+        {
+            Product ProductToUpdate = products.Find(b => b.Id == product.Id);
+            if (ProductToUpdate != null)
+            {
+                //ako postoji traženi proizvod isti mijenjamo s novim
+                ProductToUpdate = product;
+            }
+            //ako proizvod ne postoji bacamo novu iznimku
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+        //pretraživanje proizvoda prema id-u
+        public Product Find(string Id)
+        {
+            Product product = products.Find(b => b.Id == Id);
+            if (product != null)
+            {
+                //ako postoji traženi proizvod isti vračamo
+                return product;
+            }
+            //ako proizvod ne postoji bacamo novu iznimku
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+        //metoda koja vraća listu proizvoda
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Product product = products.Find(b => b.Id == Id);
+            if (product != null)
+            {
+                //ako postoji traženi proizvod isti brišemo
+                products.Remove(product);
+            }
+            //ako proizvod ne postoji bacamo novu iznimku
+            else
+            {
+                throw new Exception("Product not found");
+            }
+
+        }
+        
+    }
+}
