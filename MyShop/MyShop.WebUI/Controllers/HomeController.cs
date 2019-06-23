d73dae00dd7466f24a8222e40ecbc0836b8c935c
mladen2004 <48154793+mladen2004@users.noreply.github.com>
2019-06-23 18:16:16 +0200

    Added product list

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,36 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
+
+        //proslijeđujemo listu proizvoda u main view
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+
+            return View(products);
+        }//metoda koja ispisuje detalje o proizvodu na koji smo kliknuli u listi proizvoda
+        //navedeni action link prosljeđuje id proizvoda
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
